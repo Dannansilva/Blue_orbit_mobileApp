2a354b4f216cd423c798d91915b300495ea15ac4
Dannansilva <dannangunathilaka@gmail.com>
2024-04-03 16:26:34 +0530

    reservation page

--- blue_orbit_mobileapp/lib/Reserve_table.dart
+++ blue_orbit_mobileapp/lib/Reserve_table.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, camel_case_types
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, camel_case_types, sort_child_properties_last, avoid_unnecessary_containers, annotate_overrides
 
+import 'package:blue_orbit_mobileapp/Components/Bottom_navbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
 class reservetable extends StatefulWidget {
@@ -13,6 +15,7 @@ class reservetable extends StatefulWidget {
 class _reservetableState extends State<reservetable> {
   // date time variable
   DateTime selectedDate = DateTime.now();
+  late bool _isLunchSelected;
   //date picker
   void _showDatePicker() {
     showDatePicker(
@@ -26,6 +29,11 @@ class _reservetableState extends State<reservetable> {
                 },
               )
             });
+  }
+
+  void initState() {
+    super.initState();
+    _isLunchSelected = false;
   }
 
   @override
@@ -44,7 +52,7 @@ class _reservetableState extends State<reservetable> {
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top: 25.0),
+                  padding: const EdgeInsets.only(top: 25.0, bottom: 20),
                   child: Text(
                     "Reserve your table",
                     style: TextStyle(
@@ -58,12 +66,30 @@ class _reservetableState extends State<reservetable> {
                           ..style = PaintingStyle.stroke),
                   ),
                 ),
+                //pick date text
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0, top: 35),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "PICK A DATE",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w900,
+                          decoration: TextDecoration.none,
+                          foreground: Paint()
+                            ..strokeWidth = 1.5
+                            ..color = Colors.black
+                            ..style = PaintingStyle.stroke),
+                    ),
+                  ),
+                ),
                 //date picker
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
+                      borderRadius: BorderRadius.circular(50),
                       color: Colors.white,
                     ),
                     child: Padding(
@@ -79,6 +105,141 @@ class _reservetableState extends State<reservetable> {
                       ),
                     ),
                   ),
+                ),
+                Spacer(),
+                Column(
+                  children: [
+                    Text(
+                      "PICK A TIME",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w900,
+                          // color: Color(0xffDAA420),
+                          decoration: TextDecoration.none,
+                          foreground: Paint()
+                            ..strokeWidth = 1.5
+                            ..color = Colors.black
+                            ..style = PaintingStyle.stroke),
+                    ),
+                    //togle bar
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 50.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 8.0, right: 8, top: 5, bottom: 5),
+                          child: ToggleButtons(
+                            borderWidth: 0,
+                            borderColor: Colors.transparent,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    bottomLeft: Radius.circular(20),
+                                    topRight: Radius.circular(
+                                        20), // Added for rounded border
+                                    bottomRight: Radius.circular(
+                                        20), // Added for rounded border
+                                  ),
+                                  color: _isLunchSelected
+                                      ? Colors.black
+                                      : Colors.transparent,
+                                  border: Border.all(
+                                    color: Colors.transparent,
+                                  ),
+                                ),
+                                child: Text(
+                                  "Lunch",
+                                  style: TextStyle(
+                                    color: _isLunchSelected
+                                        ? Colors.white
+                                        : Colors.black,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(20),
+                                    bottomRight: Radius.circular(20),
+                                    topLeft: Radius.circular(
+                                        20), // Added for rounded border
+                                    bottomLeft: Radius.circular(
+                                        20), // Added for rounded border
+                                  ),
+                                  color: !_isLunchSelected
+                                      ? Colors.black
+                                      : Colors.transparent,
+                                  border: Border.all(
+                                    color: Colors.transparent,
+                                  ),
+                                ),
+                                child: Text(
+                                  "Dinner",
+                                  style: TextStyle(
+                                    color: !_isLunchSelected
+                                        ? Colors.white
+                                        : Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ],
+                            isSelected: [_isLunchSelected, !_isLunchSelected],
+                            onPressed: (index) {
+                              setState(() {
+                                _isLunchSelected = index == 0;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Row(
+                      children: [
+                        Spacer(),
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(right: 25.0, bottom: 25),
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Next",
+                                style: TextStyle(color: Color(0xff140A4D)),
+                              )),
+                        ),
+                      ],
+                    ),
+
+                    //navbar
+                    Container(
+                      // Set the background color here
+                      child: BottomNavBar(
+                        icons: [
+                          Icons.menu,
+                          Icons.local_offer,
+                          Icons.book_online,
+                          Icons.info,
+                        ],
+                        onPressed: [
+                          () => Navigator.pushNamed(context, '/intro'),
+                          () => Navigator.pushNamed(context, '/offer'),
+                          () => Navigator.pushNamed(context, '/intro2'),
+                          () => Navigator.pushNamed(context, '/guest'),
+                        ],
+                        currentIndex: 2,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
